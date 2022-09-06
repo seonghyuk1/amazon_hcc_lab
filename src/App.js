f4bfcdd301e46cafbaeb8e675e9a902dc3b6f297
seonghyuk1 <tjdgur0737@naver.com>
2022-09-06 20:02:12 +0900

    hello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+/* eslint-disable */
 import Header from "./Header";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
+import Profile from "./Profile/Profile";
+import Cart from "./Cart/Cart";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Routes>
+        <Route path='/Profile' element={<Profile/>} /> 
+        <Route path='/Cart' element={<Cart />} />
+      </Routes>
+
+
     </>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -14,11 +14,13 @@ export default function Header() {
             className={styles.Hamburger}
             style={{ color: "white" }}
           />
+          <Link to='/' className={styles.Link}>
           <img
             className={styles.HeaderImg}
             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             alt=""
           />
+          </Link>
 
           <input type="search" className={styles.SearchInput} />
           <AiOutlineSearch className={styles.SearchIcon} />
@@ -46,13 +48,19 @@ export default function Header() {
                 <b>Prime</b>
               </span>
             </div>
+
+            <Link to="/Profile" className={styles.Link}>
             <div className={styles.Next}>
               <span className={styles.Title}>Your</span>
               <span className={styles.SmallTitle}>
                 <b>Profile</b>
               </span>
             </div>
+            </Link>
+
+            <Link to="/Cart" className={styles.Link}>
             <HiShoppingCart className={styles.Cart} />
+            </Link>
           </div>
         </div>
       </header>
