8c2e9531f0fe53652541b7b98d5cfd66510b6f47
hb9901 <hb9901@gmail.com>
2022-09-18 14:21:48 +0900

    Footer, border update

--- src/Footer.js
+++ src/Footer.js
@@ -16,7 +16,7 @@ function Footer(){
             <div className="FooterList">
                 <div className="FooterComponent">
                     <div className="FooterTitle">Get to Know us</div>
-                        <ul>
+                        <ul className="FooterLi">
                         <li>About</li>
                         <li>Career</li>
                         <li>Press</li>
@@ -24,14 +24,37 @@ function Footer(){
                         <li>Gift a smile</li>
                         </ul>
                 </div>
+                <div className="Footerempty"/>
                 <div className="FooterComponent">
                     <div className="FooterTitle">Get to Know us</div>
-                        <ul>
-                        <li>About</li>
-                        <li>Career</li>
-                        <li>Press</li>
-                        <li>Amazon Cares</li>
-                        <li>Gift a smile</li>
+                        <ul className="FooterLi">
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        </ul>
+                </div>
+                <div className="Footerempty"/>
+                <div className="FooterComponent">
+                    <div className="FooterTitle">Make Money with Us</div>
+                        <ul className="FooterLi">
+                        <li>Sell on fake Amazon</li>
+                        <li>Sell under fake Amazon</li>
+                        <li>Become an Affiliate</li>
+                        <li>Fulfilment by Amazon</li>
+                        <li>Amazon Pay</li>
+                        </ul>
+                </div>
+                <div className="Footerempty"/>
+                <div className="FooterComponent">
+                    <div className="FooterTitle">Connect with us</div>
+                        <ul className="FooterLi">
+                        <li>COVID-19 and Amazon</li>
+                        <li>Your Account</li>
+                        <li>Returns Centre</li>
+                        <li>100% Purchase Protection</li>
+                        <li>Amazon App Download</li>
+                        <li>Amazon Assistant Download</li>
+                        <li>Help</li>
                         </ul>
                 </div>
             </div>
